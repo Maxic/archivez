563e7c553b678a42355cda86c969e0d57ed182cd
Barend <barend.poot@gmail.com>
2018-09-23 11:55:22 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+var example1 = new Vue({
+  el: '#games',
+  data: {
+    games: [
+      { 
+      	title: 'Timothy Leary\'s Mind Mirror' ,
+      	link: 'https://archive.org/details/msdos_Timothy_Learys_Mind_Mirror_1986', 
+      	winner: false,
+      	episode: 5,
+      },
+      { 
+      	title: 'Oxyd',
+      	link: 'https://archive.org/details/msdos_Oxyd_1992', 
+      	winner: true,
+        episode: 5,
+
+      },
+      { 
+      	title: 'Lords of Doom',
+      	link: 'https://archive.org/details/msdos_Lords_of_Doom_1990',
+      	winner: false,
+        episode: 5,
+      },
+      { 
+      	title: 'Jordan vs Bird, One-on-One',
+      	link: 'https://archive.org/details/msdos_Jordan_vs_Bird_-_One_on_One_1988', 
+      	winner: true,
+      	episode: 4,
+      },
+      { 
+      	title: 'Silicon Dreams',
+      	link: 'https://archive.org/details/msdos_Silicon_Dreams_1986', 
+      	winner: false,
+        episode: 4,
+
+      },
+      { 
+      	title: 'Spy Master',
+      	link: 'https://archive.org/details/msdos_Spy_Master_1994',
+      	winner: false,
+        episode: 4,
+      },
+      { 
+      	title: 'Graeme Souness Soccer Manager' ,
+      	link: 'https://archive.org/details/msdos_Graeme_Souness_Soccer_Manager_1992', 
+      	winner: false,
+      	episode: 3,
+      },
+      { 
+      	title: 'Webster - The Word Game',
+      	link: 'https://archive.org/details/msdos_Webster_-_The_Word_Game_1984', 
+      	winner: false,
+        episode: 3,
+
+      },
+      { 
+      	title: 'URI Quest, Chapter One: The Freshman Encounter',
+      	link: 'https://archive.org/details/agi_uri-quest-v0.173',
+      	winner: true,
+        episode: 3,
+      },
+      { 
+      	title: 'Project Neptune' ,
+      	link: 'https://archive.org/details/msdos_Project_Neptune_1989', 
+      	winner: true,
+      	episode: 2,
+      },
+      { 
+      	title: 'Lettrix',
+      	link: 'https://archive.org/details/msdos_Lettrix_1990', 
+      	winner: false,
+        episode: 2,
+
+      },
+      { 
+      	title: 'Willy The Worm',
+      	link: 'https://archive.org/details/msdos_Willy_The_Worm_1985',
+      	winner: false,
+        episode: 2,
+      },
+      { 
+      	title: 'Pyramid Power' ,
+      	link: 'https://archive.org/details/msdos_Pyramid_Power_1986', 
+      	winner: false,
+      	episode: 1,
+      },
+      { 
+      	title: 'Miner 2049er',
+      	link: 'https://archive.org/details/msdos_Miner_2049er_1983', 
+      	winner: false,
+        episode: 1,
+
+      },
+      { 
+      	title: 'Metaltech Battledrome',
+      	link: 'https://archive.org/details/msdos_Metaltech_Battledrome_1994',
+      	winner: true,
+        episode: 1,
+      },
+    ]
+  }
+})
