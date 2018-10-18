88314f53d5bac84a0fc4af2c04611d599d9da126
unknown <david.alpha@gmail.com>
2018-10-18 15:13:36 +0200

    new icons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 Vue.component('episode-list', {
 	props: ['game'],
-	template: 
+	template:
 	`
     <div class="container">
       <div class="left">
@@ -20,8 +20,8 @@ Vue.component('episode-list', {
         </div>
         <div class="three columns"/>
           <div v-if="!(game.episode % 2 == 0)">
-          <img v-if="game.winner" src="assets/FloppyCrown.png" height=128>
-          <img v-else src="assets/FloppyClean.png" height=128>
+          <img v-if="game.winner" src="assets/MS-DOS_icon_gold.png" height=128>
+          <img v-else src="assets/MS-DOS_icon.png" height=128>
           </div>
         </div>
       </div>
@@ -37,7 +37,7 @@ Vue.component('episode-list', {
 
 Vue.component('ranking-list', {
 	props: ['game'],
-	template: 
+	template:
 	`
 	<div class="container">
 	  <div v-if="game.winner" class="row">
@@ -50,7 +50,7 @@ Vue.component('ranking-list', {
           <a :href="game.link">{{ game.ranking }}. {{ game.title }}</a>
        </div>
 	 </div>
-	</div> 
+	</div>
 	`,
 })
 
@@ -94,136 +94,136 @@ var app = new Vue({
       	link: "https://archive.org/details/msdos_Thiefs_Adventure_1986",
       	winner: false,
         episode: 7,
-      	ranking: null,      
+      	ranking: null,
       },
       {
       	title: "Fallen Angel",
       	link: "https://archive.org/details/msdos_Fallen_Angel_1989",
       	winner: false,
         episode: 7,
-      	ranking: null,      	
+      	ranking: null,
       },
-      { 
+      {
       	title: 'Adventures in Melgratta',
-      	link: 'https://archive.org/details/msdos_4th_and_Inches_1988', 
+      	link: 'https://archive.org/details/msdos_4th_and_Inches_1988',
       	winner: false,
         episode: 6,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Kret' ,
-      	link: 'https://archive.org/details/msdos_Kret_1991', 
+      	link: 'https://archive.org/details/msdos_Kret_1991',
       	winner: true,
       	episode: 6,
       	ranking: 4,
       },
-      { 
+      {
       	title: '4th & Inches',
       	link: 'https://archive.org/details/game-maker_adventures-in-melgratta',
       	winner: false,
         episode: 6,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Timothy Leary\'s Mind Mirror' ,
-      	link: 'https://archive.org/details/msdos_Timothy_Learys_Mind_Mirror_1986', 
+      	link: 'https://archive.org/details/msdos_Timothy_Learys_Mind_Mirror_1986',
       	winner: false,
       	episode: 5,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Oxyd',
-      	link: 'https://archive.org/details/msdos_Oxyd_1992', 
+      	link: 'https://archive.org/details/msdos_Oxyd_1992',
       	winner: false,
         episode: 5,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Lords of Doom',
       	link: 'https://archive.org/details/msdos_Lords_of_Doom_1990',
       	winner: true,
         episode: 5,
       	ranking: 5,
       },
-      { 
+      {
       	title: 'Jordan vs Bird, One-on-One',
-      	link: 'https://archive.org/details/msdos_Jordan_vs_Bird_-_One_on_One_1988', 
+      	link: 'https://archive.org/details/msdos_Jordan_vs_Bird_-_One_on_One_1988',
       	winner: true,
       	episode: 4,
       	ranking: 3,
       },
-      { 
+      {
       	title: 'Silicon Dreams',
-      	link: 'https://archive.org/details/msdos_Silicon_Dreams_1986', 
+      	link: 'https://archive.org/details/msdos_Silicon_Dreams_1986',
       	winner: false,
         episode: 4,
       	ranking: null,
 
       },
-      { 
+      {
       	title: 'Spy Master',
       	link: 'https://archive.org/details/msdos_Spy_Master_1994',
       	winner: false,
         episode: 4,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Graeme Souness Soccer Manager' ,
-      	link: 'https://archive.org/details/msdos_Graeme_Souness_Soccer_Manager_1992', 
+      	link: 'https://archive.org/details/msdos_Graeme_Souness_Soccer_Manager_1992',
       	winner: false,
       	episode: 3,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Webster - The Word Game',
-      	link: 'https://archive.org/details/msdos_Webster_-_The_Word_Game_1984', 
+      	link: 'https://archive.org/details/msdos_Webster_-_The_Word_Game_1984',
       	winner: false,
         episode: 3,
       	ranking: null,
       },
-      { 
+      {
       	title: 'URI Quest, Chapter One: The Freshman Encounter',
       	link: 'https://archive.org/details/agi_uri-quest-v0.173',
       	winner: true,
         episode: 3,
       	ranking: 6,
       },
-      { 
+      {
       	title: 'Project Neptune' ,
-      	link: 'https://archive.org/details/msdos_Project_Neptune_1989', 
+      	link: 'https://archive.org/details/msdos_Project_Neptune_1989',
       	winner: true,
       	episode: 2,
       	ranking: 2,
       },
-      { 
+      {
       	title: 'Lettrix',
-      	link: 'https://archive.org/details/msdos_Lettrix_1990', 
+      	link: 'https://archive.org/details/msdos_Lettrix_1990',
       	winner: false,
         episode: 2,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Willy The Worm',
       	link: 'https://archive.org/details/msdos_Willy_The_Worm_1985',
       	winner: false,
         episode: 2,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Pyramid Power' ,
-      	link: 'https://archive.org/details/msdos_Pyramid_Power_1986', 
+      	link: 'https://archive.org/details/msdos_Pyramid_Power_1986',
       	winner: false,
       	episode: 1,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Miner 2049er',
-      	link: 'https://archive.org/details/msdos_Miner_2049er_1983', 
+      	link: 'https://archive.org/details/msdos_Miner_2049er_1983',
       	winner: false,
         episode: 1,
       	ranking: null,
       },
-      { 
+      {
       	title: 'Metaltech Battledrome',
       	link: 'https://archive.org/details/msdos_Metaltech_Battledrome_1994',
       	winner: true,
