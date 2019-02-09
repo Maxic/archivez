2a81efaf171407ae94a1cefa289846a961a0d7e6
David <david.alpha@gmail.com>
2019-02-09 15:13:21 +0100

    Episode list component

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
+
+
 Vue.component('episode-list', {
+	props: ['episode'],
+	template:
+	`
+	<div class="episode">
+	<div class="row">
+	<div class="three columns">
+	 <img src="assets/ephead.png">{{ episode.id }}
+	</div>
+	</div>
+	<div class="row">
+		<div class="twelve columns">
+		 <game-list v-for="game in episode.games" v-bind:game="game">
+		 </game-list>
+    </div>
+	</div>
+	</div>
+	`,
+})
+
+Vue.component('game-list', {
 	props: ['game'],
 	template:
 	`
-		<div class="row">
-     <div class="three columns">
-      <div v-if="game.episode % 2 == 0">
-       <center>
-        <img v-if="game.winner" src="assets/MS-DOS_icon_gold.png" height=68>
-        <img v-else src="assets/MS-DOS_icon.png" height=68>
-        <br><br>
-       </center>
-      </div>
-      <div v-else>
-       &nbsp;
-      </div>
-     </div>
-     <div class="six columns">
-     <br>
-	 <center><a :href="game.link">{{ game.title }}</a></center>
-     </div>
-     <div class="three columns"/>
-      <div v-if="!(game.episode % 2 == 0)">
-       <center>
-      	<img v-if="game.winner" src="assets/MS-DOS_icon_gold.png" height=68>
-      	<img v-else src="assets/MS-DOS_icon.png" height=68>
-      	<br><br>
-       </center>
-      </div>
-     </div>
-    </div>
+	<div class="row">
+		<div class ="three columns">
+		<center>
+		 <img v-if="game.winner" src="assets/MS-DOS_icon_gold.png" height=68>
+		 <img v-else src="assets/MS-DOS_icon.png" height=68>
+		</center>
+		</div>
+		<div class="six columns">
+			<center><a :href="game.link">{{ game.title }}</a></center>
+		</div>
+		</div>
 	`,
 })
 
@@ -57,52 +63,55 @@ var app = new Vue({
   el: '#app',
   data: {
   	toggleEpisode : true,
-		episodes: [{
-			epnr: 9,
-			games: [
-					  {
-							title: "DEMO .EXE (Obscure MS-DOS Game)",
-							link: "https://archive.org/details/demoexe",
-							winner: true,
-							episode: 9,
-							ranking: 9,
-						}, {
-							title: "Tag Team Wrestling",
-							link: "https://archive.org/details/msdos_Tag_Team_Wrestling_1986",
-							winner: false,
-							episode: 9,
-							ranking: null,
-						}, {
-							title: "Druglord",
-							link: "https://archive.org/details/msdos_Druglord_1991",
-							winner: false,
-							episode: 9,
-							ranking: null,
-						}
-				]
-			},
-			{games: [
-						{
-							title: "NARCS",
-							link: "https://archive.org/details/demoexe",
-							winner: true,
-							episode: 10,
-							ranking: 9,
-						}, {
-							title: "Burt Beach Boys 2",
-							link: "https://archive.org/details/msdos_Tag_Team_Wrestling_1986",
-							winner: false,
-							episode: 10,
-							ranking: null,
-						}, {
-							title: "Goodlord XI",
-							link: "https://archive.org/details/msdos_Druglord_1991",
-							winner: false,
-							episode: 10,
-							ranking: null,
-						}
-			]}
-		]
+		episodes: [
+								{ id: 9,
+								  games: [
+												  {
+														title: "DEMO .EXE (Obscure MS-DOS Game)",
+														link: "https://archive.org/details/demoexe",
+														winner: true,
+														episode: 9,
+														ranking: 9,
+													}, {
+														title: "Tag Team Wrestling",
+														link: "https://archive.org/details/msdos_Tag_Team_Wrestling_1986",
+														winner: false,
+														episode: 9,
+														ranking: null,
+													}, {
+														title: "Druglord",
+														link: "https://archive.org/details/msdos_Druglord_1991",
+														winner: false,
+														episode: 9,
+														ranking: null,
+													}
+												]
+
+								},
+								{ id: 10,
+									games: [
+													{
+														title: "NARCS",
+														link: "https://archive.org/details/demoexe",
+														winner: true,
+														episode: 10,
+														ranking: 9,
+													}, {
+														title: "Burt Beach Boys 2",
+														link: "https://archive.org/details/msdos_Tag_Team_Wrestling_1986",
+														winner: false,
+														episode: 10,
+														ranking: null,
+													}, {
+														title: "Goodlord XI",
+														link: "https://archive.org/details/msdos_Druglord_1991",
+														winner: false,
+														episode: 10,
+														ranking: null,
+													}
+												]
+								}
+							]
 
 },
   computed: {
